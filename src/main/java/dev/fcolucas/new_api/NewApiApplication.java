6cf6030a894388428689d288f04097f59c3cfe4c
Francisco Lucas <fcolucaslima14@gmail.com>
2024-07-29 20:03:23 -0300

    bugfix cors

--- src/main/java/dev/fcolucas/new_api/NewApiApplication.java
+++ src/main/java/dev/fcolucas/new_api/NewApiApplication.java
@@ -3,6 +3,10 @@ package dev.fcolucas.new_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default server URL") })
 @SpringBootApplication
 public class NewApiApplication {
 
